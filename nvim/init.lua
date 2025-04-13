1a876d9dd517433ce1f10b3ddf8776e27bf11157
pechorka <pechor44@gmail.com>
2025-04-13 15:03:55 +0500

    ignore case on search. sync vim and system clipboards

--- nvim/init.lua
+++ nvim/init.lua
@@ -31,6 +31,41 @@ vim.opt.updatetime = 50
 -- Show a vertical line at column 80
 vim.opt.colorcolumn = "120"
 
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.opt.confirm = true
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- sync vim and system clipboard
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
 -- netrw
 vim.g.netrw_liststyle = 3
 vim.g.netrw_banner = 0
@@ -48,7 +83,7 @@ vim.keymap.set('n', 'K', lsp.hover, { desc = 'Show documentation' })
 vim.keymap.set('n', '<leader>d', lsp.diagnostics, { desc = 'Show diagnostics' })
 local display = require('plugins.prelude.list_files.init')
 
-vim.keymap.set('n', '<leader>ff', function()
+vim.keymap.set('n', '<leader>f', function()
     local cwd = vim.fn.getcwd()
     local files = {}
 
