564ceda9e4322118cdabec7b4ff2a04b7fd5a886
pechorka <pechor44@gmail.com>
2026-02-20 21:30:39 +0500

    remove depreacted keychain arg

--- fish/functions/__ensure_ssh_agent.fish
+++ fish/functions/__ensure_ssh_agent.fish
@@ -10,9 +10,9 @@ function __ensure_ssh_agent --description 'Ensure ssh-agent is running and keys 
     if type -q keychain
         set -l keys (__ssh_candidate_keys)
         if test (count $keys) -gt 0
-            keychain --quiet --agents ssh --eval $keys | source
+            keychain --quiet --eval $keys | source
         else
-            keychain --quiet --agents ssh --eval | source
+            keychain --quiet --eval | source
         end
         return
     end
